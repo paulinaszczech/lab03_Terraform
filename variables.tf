dc3057639f2722bbe726862752a0ef7d9dd5f6f9
Paulina <paulina.szczech89@gmail.com>
2025-04-27 15:21:10 +0200

    add

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "github_token" {
+  description = "GitHub Personal Access Token"
+  type        = string
+  sensitive   = true # Marks this variable as sensitive, preventing it from appearing in logs and console output
+}
+
+variable "repository_name" {
+  description = "Name of the GitHub repository to create"
+  type        = string
+  default     = "terraform-managed-repo"
+}
+
+variable "repository_description" {
+  description = "Description of the GitHub repository"
+  type        = string
+  default     = "Repository managed by Terraform"
+}
